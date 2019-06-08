94b4ee95a962084136d39d33f10618c2c156759c
ThiagoC&C <51183849+CompreCompare@users.noreply.github.com>
2019-06-07 21:22:17 -0300

    small update

--- js/login.js
+++ js/login.js
@@ -1,10 +1,9 @@
 function logar(){
-    var log=document.form1.txtEmail.value;
+    var log=document.form1.txtLogin.value;
     var senha=document.form1.pswSenha.value;
     if (log=="Larissa@gmail.com" && senha=="4321") {
-    	window.location.replace("produtos/user1.html");
-    }else if (log=="Jian_teteu@outlok.com" && senha=="abc123") {
-    	window.location.replace("produtos/user1.html");
+    	window.location.replace("../produtos/user1.html");
+    }else if (log=="Jian_mateus@outlook.com" && senha=="abc123") {
+    	window.location.replace("../produtos/user2.html");
     }
-               
 }
